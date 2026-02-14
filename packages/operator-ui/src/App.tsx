78b3251aabcc0e4e396a42bb699a8cbf04c5aafd
Gillian Tunney <gbtunney@mac.com>
2026-02-14 03:16:59 +0000

    fix(ui): redid ui vite

--- packages/operator-ui/src/App.tsx
+++ packages/operator-ui/src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
+import { ReactElement, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+function App(): ReactElement {
     const [count, setCount] = useState(0)
 
     return (
@@ -18,7 +18,7 @@ function App() {
             </div>
             <h1>Vite + React</h1>
             <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+                <button onClick={() => { setCount((count) => count + 1); }}>count is {count}</button>
                 <p>
                     Edit <code>src/App.tsx</code> and save to test HMR
                 </p>
