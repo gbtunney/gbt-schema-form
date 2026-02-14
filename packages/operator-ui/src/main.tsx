e630d10855bc7beddcb7ea857ba6b00cd4837090
Gillian Tunney <gbtunney@mac.com>
2026-02-14 05:13:04 +0000

    feat(ui): added storybook to ui

--- packages/operator-ui/src/main.tsx
+++ packages/operator-ui/src/main.tsx
@@ -3,7 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
+const doc = document.getElementById('root')
+if (doc === null) {
+    throw new Error('Root element not found')
+}
+createRoot(doc).render(
     <StrictMode>
         <App />
     </StrictMode>,
